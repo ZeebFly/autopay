3f428380477d690553e01d4c92662267c66ad767
ZeebFly <zeebrobot@gmail.com>
2026-01-20 20:49:14 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,36 +1,27 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
-from payment import create_qris
-from config import BOT_TOKEN, PRICE
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
+
+BOT_TOKEN = "ISI_TOKEN_BOT"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
         [InlineKeyboardButton("🛒 Beli Akses", callback_data="buy")]
     ]
     await update.message.reply_text(
-        "👋 Selamat datang\n\n"
-        "Akses Group Private\n"
-        f"Harga: Rp {PRICE:,}",
+        "Selamat datang\n\nKlik tombol di bawah untuk beli akses grup",
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
 async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    q = update.callback_query
-    await q.answer()
+    query = update.callback_query
+    await query.answer()
 
-    if q.data == "buy":
-        qr = create_qris(q.from_user.id)
-        await q.message.reply_photo(
-            photo=qr,
-            caption=(
-                f"💳 Silakan bayar\n"
-                f"Jumlah: Rp {PRICE:,}\n\n"
-                "Setelah pembayaran berhasil,\n"
-                "link akses akan dikirim otomatis."
-            )
+    if query.data == "buy":
+        await query.edit_message_text(
+            "Harga: Rp10.000\n\nSilakan lanjutkan pembayaran"
         )
 
-app = ApplicationBuilder().token(BOT_TOKEN).build()
+app = Application.builder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(button))
 
